3f4f94ffb95e2afcc6ce1d707900bd1e45ca5852
Ben Nageris <nageris@amazon.com>
2025-05-30 19:53:29 +0300

    add env wrappers goal lists and env registration

--- gr_envs/wrappers/panda_wrapper.py
+++ gr_envs/wrappers/panda_wrapper.py
@@ -1,21 +1,66 @@
-from .goal_wrapper import GoalRecognitionWrapper
+import random
+from typing import Optional
+
+import gymnasium
+from gymnasium.spaces import Box
+
+from gr_envs.wrappers.goal_wrapper import GoalRecognitionWrapper, Goal
 from .hook import get_property_reference
-import highway_env
 import numpy as np
+
+class PandaGymDesiredGoalList(Goal):
+	def __init__(self, goals: list[np.ndarray]):
+		assert type(goals) == list, f"goals should be with list type, not {type(goals)}"
+		self.goals = goals
+		self.current_goal = random.choice(self.goals)
+
+	def get(self) -> np.ndarray:
+		return self.current_goal
+
+	def reset(self) -> None:
+		self.current_goal = random.choice(self.goals)
 
 class PandaGymWrapper(GoalRecognitionWrapper):
 	GOAL_DIMENSION_SHAPE = (3,)
 	GOAL_DTYPE = np.float32
 	HOOK_FUNC = "_sample_goal"
-	def __init__(self, env, desired_goal):
-		super().__init__(env, name="panda")
-
-		assert desired_goal.dtype == PandaGymWrapper.GOAL_DTYPE
-		assert desired_goal.shape == PandaGymWrapper.GOAL_DIMENSION_SHAPE
+	def __init__(self, env: gymnasium.Env, goal: Optional[PandaGymDesiredGoalList], action_space: Box):
+		super().__init__(env, name="panda", goal=goal)
 
 		hooked_env = get_property_reference(env, PandaGymWrapper.HOOK_FUNC)
 		setattr(
 			hooked_env,
 			PandaGymWrapper.HOOK_FUNC,
-			lambda: np.array(desired_goal, dtype=self.env.observation_space['desired_goal'].dtype)
+			lambda: self._reset_goals()
 		)
+
+		hooked_robot_env = get_property_reference(env, "robot")
+		setattr(
+			hooked_robot_env.robot,
+			"action_space",
+			action_space
+		)
+		setattr(
+			hooked_robot_env,
+			"action_space",
+			action_space
+		)
+		self._hooked_robot_env = hooked_robot_env
+
+	def _reset_goals(self) -> np.ndarray:
+		if self.goal is None:
+			goal = self.env.np_random.uniform(
+				self._hooked_robot_env.task.goal_range_low, self._hooked_robot_env.task.goal_range_high
+			)
+			return goal
+		else:
+			self.goal.reset()
+			return np.array(self.goal.get(), dtype=self.env.observation_space['desired_goal'].dtype)
+
+	@staticmethod
+	def goal_to_str(goal: np.ndarray) -> str:
+		goal_str = "X".join(
+			[f"{float(g):.3g}".replace(".", "y").replace("-", "M") for g in goal]
+		)
+		print(f"Goal string: {goal_str}")
+		return goal_str
