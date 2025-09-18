9396316f1dc5df94289861a6fbe4cfd3c05486d7
Matan Shamir <matansh@mobileye.com>
2025-09-18 14:56:23 +0300

    change import from the import to be inside try clause to prevent falling if some env wasn't installed

--- gr_envs/register.py
+++ gr_envs/register.py
@@ -1,20 +1,18 @@
-from gr_envs.env_registration.panda_register import panda_gym_register
-from gr_envs.env_registration.highway_register import register_highway_envs
-from gr_envs.env_registration.maze_register import point_maze_register
-
-
 def register_all_envs():
     try:
+        from gr_envs.env_registration.panda_register import panda_gym_register
         panda_gym_register()
     except Exception as e:
-        print(f"Panda-Gym registration failed, {e}")
+        print(f"Panda-Gym registration failed, {e}. Ignore this warning if you didn't install gr_envs[panda] extra.")
 
     try:
+        from gr_envs.env_registration.highway_register import register_highway_envs
         register_highway_envs()
     except Exception as e:
-        print(f"Highway-Env registration failed, {e}")
+        print(f"Highway-Env registration failed, {e}. Ignore this warning if you didn't install gr_envs[highway] extra.")
 
     try:
+        from gr_envs.env_registration.maze_register import point_maze_register
         point_maze_register()
     except Exception as e:
-        print(f"Point-Maze registration failed, {e}")
+        print(f"Point-Maze registration failed, {e}. Ignore this warning if you didn't install gr_envs[maze] extra.")
