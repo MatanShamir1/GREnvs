71f6100172b24c47530d5945db0d252831618773
Matan Shamir <matansh@mobileye.com>
2025-09-18 19:59:51 +0300

    safe imports of wrappers to prevent failure if maze extra is installed but highway is not

--- gr_envs/wrappers/__init__.py
+++ gr_envs/wrappers/__init__.py
@@ -1,3 +1,16 @@
-from gr_envs.wrappers.parking_wrapper import ParkingWrapper, ParkingGoalList
-from gr_envs.wrappers.panda_wrapper import PandaGymWrapper, PandaGymDesiredGoalList
-from gr_envs.wrappers.goal_wrapper import GoalRecognitionWrapper
+import warnings
+
+try:
+    from gr_envs.wrappers.parking_wrapper import ParkingWrapper, ParkingGoalList
+except ImportError:
+    warnings.warn("ParkingWrapper or ParkingGoalList could not be imported. Install gr_envs[highway] if needed.", RuntimeWarning)
+
+try:
+    from gr_envs.wrappers.panda_wrapper import PandaGymWrapper, PandaGymDesiredGoalList
+except ImportError:
+    warnings.warn("PandaGymWrapper or PandaGymDesiredGoalList could not be imported. Install gr_envs[panda] if needed.", RuntimeWarning)
+
+try:
+    from gr_envs.wrappers.goal_wrapper import GoalRecognitionWrapper
+except ImportError:
+    warnings.warn("GoalRecognitionWrapper could not be imported.", RuntimeWarning)
